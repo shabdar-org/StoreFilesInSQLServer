5237dee631ef02f4474cbab10fc7653a6a35863f
shabdar-org <ghata2002@gmail.com>
2021-09-29 16:26:21 -0400

    Initial commit

--- StoreFilesInSQLServer/Program.cs
+++ StoreFilesInSQLServer/Program.cs
@@ -11,7 +11,7 @@ namespace StoreFilesInSQLServer
         /// </summary>
         [STAThread]
         static void Main()
-        {
+        { 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmFilesStore());
